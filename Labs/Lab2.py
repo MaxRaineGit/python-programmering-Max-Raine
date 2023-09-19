b1e1a295064f10d5d274cc95840066938e62b109
Max Raine <max.raine03@gmail.com>
2023-09-19 19:48:19 +0200

    defined the function for euclidean distance

--- Labs/Lab2.py
+++ Labs/Lab2.py
@@ -1,16 +1,7 @@
-
+import math
 import matplotlib.pyplot as plt
 import csv
 
-
-##with open("Labs\\datapoints.csv", "r") as datafile:
- #   datapoints = pd.read_csv(datafile, delimiter=",")
- #   data = pd.DataFrame(datapoints)
- #   x_data = data['(width (cm)']
-  #  y_data = data[' height (cm)']
-    
-##data = data.drop(' 1-pikachu))', axis=1)
-#data = data.rename(columns={"(width (cm)": "Width", " height (cm)": "Height", " label (0-pichu": "Class"})
 
 pichu_training = []
 pikachu_training = []
@@ -25,8 +16,7 @@ with open("Labs\\datapoints.txt", "r") as file:
         if label == 1:
             pikachu_training.append([width, height])
 
-    #for row in data:
-        #training.append({"Width": row["(width (cm)"], "Height": row[" height (cm)"], "Class": row[" label (0-pichu"]})
+   
 
 
 
@@ -34,4 +24,5 @@ plt.scatter([x[0] for x in pichu_training], [y[1]for y in pichu_training], color
 plt.scatter([x[0] for x in pikachu_training], [y[1]for y in pikachu_training], color = 'blue')
 plt.show()
 
-
+def distance(point1, point2):
+    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
