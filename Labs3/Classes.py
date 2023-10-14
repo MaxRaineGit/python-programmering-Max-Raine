2d1726e0a317432b8210a7547544409c1d88f86f
Max Raine <max.raine03@gmail.com>
2023-10-14 16:10:27 +0200

    added parent class shape and operationoverload

--- Labs3/Classes.py
+++ Labs3/Classes.py
@@ -1,8 +1,50 @@
 # To get access to math.pi to calculate circle area and omkrets
 import math
 
-class Rectangle:
+# Added a parent class so i can use isinstance to limit comparing between just rectangle and circle and raise an error if tried otherwise
+class Shape:
+    def __init__(self):
+        pass
+
+    @property
+    def area(self):
+        pass
+
+    @property
+    def omkrets(self):
+        pass
+
+    def __eq__(self, other):
+        if isinstance(other, Shape):
+            return (self.length, self.width) == (other.length, other.width)
+        return False
+    
+    def __lt__(self, other):
+        if isinstance(other, Shape):
+            return (self.area, self.omkrets) < (other.area, other.omkrets)
+        raise ValueError("Please only compare objects from the class Shape or it's subclasses")
+    
+    def __le__(self, other):
+        if isinstance(other, Shape):
+            return (self.area, self.omkrets) <= (other.area, other.omkrets)
+        raise ValueError("Please only compare objects from the class Shape or it's subclasses")
+    
+    def __gt__(self, other):
+        if isinstance(other, Shape):
+            return (self.area, self.omkrets) > (other.area, other.omkrets)
+        raise ValueError("Please only compare objects from the class Shape or it's subclasses")
+    
+    def __ge__(self, other):
+        if isinstance(other, Shape):
+            return (self.area, self.omkrets) >= (other.area, other.omkrets)
+        raise ValueError("Please only compare objects from the class Shape or it's subclasses")
+        
+
+
+
+class Rectangle(Shape):
     def __init__(self, length, width):
+        super().__init__
         self.length = length
         self.width = width
 
@@ -15,14 +57,11 @@ class Rectangle:
     def omkrets(self):
         return (self.length + self.width) * 2
     
-    def __eq__(self, other):
-        if isinstance(other, Rectangle):
-            return (self.length, self.width) == (other.length, other.width)
-        return False
     
 
-class Circle:
+class Circle(Shape):
     def __init__(self, radius):
+        super().__init__
         self.radius = radius
 
     @property
@@ -33,7 +72,3 @@ class Circle:
     def omkrets(self):
         return math.pi * self.radius * 2
     
-    def __eq__(self, other):
-        if isinstance(other, Circle):
-            return self.radius == other.radius
-        return False
