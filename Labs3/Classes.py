b4fbb5443d8c5cdaf571b89d803e5a592e5aacf9
Max Raine <max.raine03@gmail.com>
2023-10-17 11:52:48 +0200

    added a move_point function and is_point_inside

--- Labs3/Classes.py
+++ Labs3/Classes.py
@@ -3,8 +3,9 @@ import math
 
 # Added a parent class so i can use isinstance to limit comparing between just rectangle and circle and raise an error if tried otherwise
 class Shape:
-    def __init__(self):
-        pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
     @property
     def area(self):
@@ -13,6 +14,10 @@ class Shape:
     @property
     def omkrets(self):
         pass
+
+    def move_point(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
     
     def __lt__(self, other):
         if isinstance(other, Shape):
@@ -38,8 +43,8 @@ class Shape:
 
 
 class Rectangle(Shape):
-    def __init__(self, length, width):
-        super().__init__
+    def __init__(self, x, y, length, width):
+        super().__init__(x, y)
         self.length = length
         self.width = width
 
@@ -57,11 +62,18 @@ class Rectangle(Shape):
             return (self.length, self.width) == (other.length, other.width)
         return False
     
+    def is_point_inside(self, x, y):
+        return (
+            self.x - self.length / 2 <= x <= self.x - self.length / 2
+            and 
+            self.y - self.width / 2 <= y <= self.y - self.width / 2
+        )
+    
     
 
 class Circle(Shape):
-    def __init__(self, radius):
-        super().__init__
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
         self.radius = radius
 
     @property
@@ -76,4 +88,8 @@ class Circle(Shape):
         if isinstance(other, Shape):
             return (self.radius) == (other.radius)
         return False
+    
+    def is_point_inside(self, x, y):
+        distance = math.sqrt((self.x - x) ** 2 + (self.y - y) ** 2)
+        return distance <= self.radius
     
